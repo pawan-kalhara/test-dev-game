5744f228a8389a4fc5e65181cdc073f05190a784
Pawan Kalhara <pawankalhara926@gmail.com>
2025-12-10 07:05:25 +0530

    Refactor creds on firebase

--- src/firebase.js
+++ src/firebase.js
@@ -9,10 +9,12 @@ const firebaseConfig = {
   storageBucket: "monkeyquest-2d-game.firebasestorage.app",
   messagingSenderId: "11649577414",
   appId: "1:11649577414:web:ffbf63078143aa36f982c7",
-  measurementId: "G-CXWGGZFPPW"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export default app;
